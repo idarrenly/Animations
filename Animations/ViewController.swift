5a1559a2e93ac2ceb82977b827a1a8694b1e9921
dly <dly@mbp.local>
2017-10-13 19:10:42 -0700

    Simple animation that moves to next frame when NEXT button is clicked. Images were converted to .png format.

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var counter = 0
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        
+        imageView.image = UIImage(named: "WonderWoman_\(counter)")
+        counter += 1
+        if counter == 23 {
+            counter = 0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
